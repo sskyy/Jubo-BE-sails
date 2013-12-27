7d7a0fa3682d3bf0df1b2e2420a55cf33e9d07c7
sskyy <skyking_H@hotmail.com>
2013-12-27 12:26:38 +0800

    half add google+ login

--- api/controllers/OauthController.js
+++ api/controllers/OauthController.js
@@ -214,6 +214,102 @@ module.exports = {
 			})
 
 		})
+	},
+	google:function(req,res){
+		var code = req.query.code,
+		    client_id = "684503587554-ln51ean53ghos3culpjvh5ehggjvh6bd.apps.googleusercontent.com",
+		    secret = "NsIDa99cer39Gw8vGtJMoJcM"
+
+		if( !code ){
+			return  res.send(401,"user not authorized.")
+		}
+
+		request({
+			uri : 'https://accounts.google.com/o/oauth2/token',
+			method : "POST",
+			form:{
+				client_id:client_id,
+				client_secret:secret,
+				redirect_uri:encodeURIComponent("http://www.buxiache.com:1337/oauth/google"),
+				grant_type:"authorization_code",
+				code:code
+			}
+		},function( err, response, body){
+			if( err ){
+				return res.send(401,{msg:"sending request for access token failed",err:err})
+			}
+
+			body = JSON.parse( body )
+			if( body.code ){
+				return res.send(401,{msg:body.msg})
+			}
+
+			// console.log("get access_token",body.access_token)
+
+			//get user info
+			request({
+				url : "https://www.googleapis.com/plus/v1/people/me",
+				headers : {
+			  		"Authorization": "Bearer "+body.access_token
+				}
+			},function( err, response, body){
+				if( err){
+			  		return res.send(401,{msg:"get user info failed",err:err})
+				}
+
+				var user= JSON.parse( body)
+				if( user.code ){
+			  		return res.send(401,{msg:user.msg})
+				}
+
+
+				var snsId= "google"+user.id
+				Oauth.findOne({snsId:snsId},function(err,oauthUser){
+					if( oauthUser ){
+						console.log("user already logined with google")
+						User.findOne(oauthUser.localId,function(err, localUser){
+							if( localUser ){
+								console.log("found local user",JSON.stringify(localUser))
+								req.session.user = localUser
+								res.redirect("http://www.buxiache.com:9000")
+							}else{
+								res.send(404,"record error, local user not found")
+								//TODO deal with data mistake
+							}
+						})
+					}else{
+						console.log("new user logined with google")
+
+						var newUser = _.pick( user, 'name','avatar')
+						newUser.sign = user.desc
+						newUser.sns = {
+							google : user.alt
+						}
+						newUser.lastLogin = new Date()
+
+						User.create(newUser).done(function(err,localUser){
+							if( err ){
+								return res.send(500,"create local user failed")
+							}
+							user.snsId = "google" + user.id
+							user.localId = localUser.id
+							Oauth.create( user,function(err, oauthUser){
+								if( err ){
+									return res.send(500,"create oauth user failed")
+								}
+								console.log("all done")
+								req.session.user = localUser
+								res.redirect("http://www.buxiache.com:9000")
+							})
+
+						})
+					}
+				})
+
+
+			})
+
+		})
 	}
 };
 
